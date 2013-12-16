3c20cf746155816e572f52f44460ef611572068c
inspira <webui@inspira.(none)>
2013-12-16 23:48:12 +0200

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,42 +1,135 @@
-set :stage, :production
+#--set :application, "set your application name here"
+#--set :repository,  "set your repository location here"
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#--role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#--role :app, "your app-server here"                          # This may be the same as your `Web` server
+#--role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#--role :db,  "your slave db-server here"
 
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
 
-# fetch(:default_env).merge!(rails_env: :production)
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+# По умолчанию для дистрибуции проектов используется Bundler.
+# Эта строка включает автоматическое обновление и установку
+# недостающих gems, указанных в вашем Gemfile.
+#
+## !!! Не забудьте добавить
+# gem 'capistrano'
+# gem 'unicorn'
+#
+# в ваш Gemfile.
+#
+# Если вы используете другую систему управления зависимостями,
+# закомментируйте эту строку.
+require 'bundler/capistrano'
+#require 'sidekiq/capistrano'
+
+## Чтобы не хранить database.yml в системе контроля версий, поместите
+## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
+## следующие строки.
+
+# after "deploy:update_code", :copy_database_config
+# task :copy_database_config, roles => :app do
+#   db_config = "#{shared_path}/database.yml"
+#   run "cp #{db_config} #{release_path}/config/database.yml"
+# end
+
+# В rails 3 по умолчанию включена функция assets pipelining,
+# которая позволяет значительно уменьшить размер статических
+# файлов css и js.
+# Эта строка автоматически запускает процесс подготовки
+# сжатых файлов статики при деплое.
+# Если вы не используете assets pipelining в своем проекте,
+# или у вас старая версия rails, закомментируйте эту строку.
+load 'deploy/assets'
+
+# Для удобства работы мы рекомендуем вам настроить авторизацию
+# SSH по ключу. При работе capistrano будет использоваться
+# ssh-agent, который предоставляет возможность пробрасывать
+# авторизацию на другие хосты.
+# Если вы не используете авторизацию SSH по ключам И ssh-agent,
+# закомментируйте эту опцию.
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+# Имя вашего проекта в панели управления.
+# Не меняйте это значение без необходимости, оно используется дальше.
+set :application,     "simpleblog"
+#set :deploy_via, :copy
+# Сервер размещения проекта.
+set :deploy_server,   "fluorine.locum.ru"
+
+# Не включать в поставку разработческие инструменты и пакеты тестирования.
+set :bundle_without,  [:development, :test]
+
+set :user,            "hosting_premierdental"
+set :login,           "premierdental"
+set :use_sudo,        false
+set :deploy_to,       "/home/#{user}/projects/#{application}"
+set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
+set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
+set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
+role :web,            deploy_server
+role :app,            deploy_server
+role :db,             deploy_server, :primary => true
+
+# Следующие строки необходимы, т.к. ваш проект использует rvm.
+set :rvm_ruby_string, "ruby-2.0.0-p353"
+set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake"
+set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
+
+# Настройка системы контроля версий и репозитария,
+# по умолчанию - git, если используется иная система версий,
+# нужно изменить значение scm.
+set :scm,             :git
+
+# Предполагается, что вы размещаете репозиторий Git в вашем
+# домашнем каталоге в подкаталоге git/<имя проекта>.git.
+# Подробнее о создании репозитория читайте в нашем блоге
+# http://locum.ru/blog/hosting/git-on-locum
+set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
+
+## Если ваш репозиторий в GitHub, используйте такую конфигурацию
+# set :repository,    "git@github.com:username/project.git"
+
+## --- Ниже этого места ничего менять скорее всего не нужно ---
+
+before 'deploy:finalize_update', 'set_current_release'
+task :set_current_release, :roles => :app do
+    set :current_release, latest_release
+end
+
+  set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+
+
+# - for unicorn - #
+namespace :deploy do
+  desc "Start application"
+  task :start, :roles => :app do
+    run unicorn_start_cmd
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
+  end
+end
